6b2b1e3431d692e9ab0f393285490db886a5cd66
nquockhanh0209 <60789588+nquockhanh0209@users.noreply.github.com>
2021-12-26 04:06:41 +0700

    Update Home.js
    add charts

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Barchart from '../components/Barchart';
+import Linechart from '../components/Linechart';
+
 
 function Home() {
-    return (
-        <div className='home'>
-            <h1>Hello</h1>
-        </div>
-    )
+  return (
+    <div className='home'>
+      <div className='map'>Đây là map</div>
+      <div className='chart'>
+        <div><Barchart /></div>
+        <div><Linechart /></div> 
+        <div><PieChart /></div> 
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
